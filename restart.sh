06c0bcb3d2df884e32e8de933fcc48b1ab0f1a7b
Laurent JULIENNE <lockeyproduction@gmail.com>
2022-01-07 19:35:11 +0100

    new script

--- restart.sh
+++ restart.sh
@@ -2,10 +2,16 @@
 pm2 stop all
 kill $(lsof -t -i:3000)
 pkill node
+#sleep 2
+#pm2 start ../nodeFinder/
+#sleep 2
 npm start
 sleep 30
 cd liveTrade
-node tradeCustomTokens.js
+#node tradeCustomTokens.js
 sleep 30
 node tradeBestTokens.js
-#node tradeAllCoins.js
+node tradeAllCoins.js
+
+
+
